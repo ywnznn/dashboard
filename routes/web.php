3d24a1041ebd89fb15b893d6ffb72c7ed5cc54e2
ywnznn <114546097+ywnznn@users.noreply.github.com>
2023-05-14 13:48:20 +0700

    templating

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('layouts/main');
+});
+Route::get('/dashboard', function () {
+    return view('admin/dashboard');
 });
