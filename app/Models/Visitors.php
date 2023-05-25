2d9b47f90c8713bd507be7e4fad844b2bc921846
ersandra <arearesting@gmail.com>
2023-05-25 23:59:37 +0700

    visitor for page landing

---
+++ app/Models/Visitors.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Visitors extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_visitors';
+
+    protected $fillable = [
+        'ip_address',
+        'browser',
+        'os',
+    ];
+}
